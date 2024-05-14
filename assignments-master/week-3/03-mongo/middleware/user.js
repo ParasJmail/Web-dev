857c1cf160a3b444cd283cabe5d35bbf0107269f
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-14 22:24:01 +0530

    week-3 03-mongo assignment done

--- assignments-master/week-3/03-mongo/middleware/user.js
+++ assignments-master/week-3/03-mongo/middleware/user.js
@@ -1,6 +1,36 @@
-function userMiddleware(req, res, next) {
+const {User} = require("../db/index")
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    // User.findOne({
+    //     username:username,
+    //     password:password
+    // }).then(function(value){
+    //     if(value){
+    //         next();
+    //     }else{
+    //         res.status(403).json({
+    //             msg:"User doesnt exist"
+    //         })
+    //     }
+    // })
+
+    const value = await User.findOne({
+        username:username,
+        password:password
+    })
+    if(value){
+        next()
+    }else{
+        res.status(403).json({
+            msg:"User doesnt exist"
+        })
+    }
 }
 
 module.exports = userMiddleware;
--- assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,59 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const {Admin,Course}  = require("../db/index")
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check if a user with this username already exists
+    await Admin.create({
+        username:username,
+        password:password
+    })
+
+    res.json({
+        message:"Admin Created Successfully"
+    })
+
+    
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCourse = await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+
+    res.json({
+        message: 'Course created Successfully',courseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    // Course.find({})
+    //     .then(function(response){
+    //         response.json({
+    //             courses: response
+    //         })
+    //     })
+
+    const allCourses = await Course.find({});
+    res.json({
+        courses: allCourses
+    })
 });
 
 module.exports = router;
