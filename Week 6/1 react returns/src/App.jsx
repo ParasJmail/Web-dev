8d7a4078b5d68bfe22e6ad6a7a959968079bcf10
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-23 14:40:33 +0530

    react_returns

---
+++ Week 6/1 react returns/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {Header} from "./components/Header.jsx"
+
+
+function App() {
+
+
+  return (
+      <React.Fragment>
+        <Header title="paras" />
+        <Header title="paras" />
+      </React.Fragment>
+  )
+}
+// function Header({title}){
+//   return(
+//     <div>
+//       {title}
+//     </div>
+//   )
+// }
+
+export default App
