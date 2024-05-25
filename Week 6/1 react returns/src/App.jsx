bb5f0deb3970edc7ad0d2fd2dd87d4e9427290f1
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-25 16:35:01 +0530

    intro to rerender

--- Week 6/1 react returns/src/App.jsx
+++ Week 6/1 react returns/src/App.jsx
@@ -1,15 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import {Header} from "./components/Header.jsx"
 
 
 function App() {
 
+  const [title,setTitle] = useState("my name is paras");
+
+  function updateTitle(){
+    setTitle("my name is " + Math.random());
+  }
 
   return (
-      <React.Fragment>
+      <div>
+        <button onClick={updateTitle}>Change title</button>
+        <Header title={title} />
         <Header title="paras" />
         <Header title="paras" />
-      </React.Fragment>
+        <Header title="paras" />
+        <Header title="paras" />
+        <Header title="paras" />
+      </div>
   )
 }
 // function Header({title}){
